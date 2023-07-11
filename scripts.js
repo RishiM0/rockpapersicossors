70ef03ee62dbaef95011c0306d8d0485af6d98c7
RishiM0 <rishi.mantri0803@gmail.com>
2023-07-11 13:11:24 -0400

    probably won't work on this for a while, did some dumb mistakes and it seems the next project is both more fun and helpful, fixing the misunderstandings I had about the game here is just tedious. Come back later if you wish, this is not necessary

--- scripts.js
+++ scripts.js
@@ -19,45 +19,119 @@ function playRound(playerSelection, computerSelection) {
         //return ("Tie! " + playerSelection + " cancels " + computerSelection + "!")
         return 0;
     } else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        return 1;
         //return ("Win! " + playerSelection + " beats " + computerSelection + "!")
+        return 1;
     } else {
-        return 2;
         //return ("Lose! " + playerSelection + " loses to " + computerSelection + "!")
+        return 2;
     }
 }
 
-function points(num) {
-    if (num == 0) {
-        return -1
-    }
-}
 
 function game() {
-    let person = 0;
-    let comp = 0;
-    for (i = 0; i < 5; i++){
-        let playerSelection = prompt("rock paper or scissors?");
+    console.log('got here');
+    let point = 0;
+    let result;
+    const rockpress = document.querySelector(".rockbutton");
+    const paperpress = document.querySelector(".paperbutton");
+    const scissorspress = document.querySelector(".scissorsbutton");
+    rockpress.addEventListener('click', () => {
+        console.log('got here too');
+        let playerSelection = 'rock';
         let computerSelection = getComputerChoice();
         if (playRound(playerSelection, computerSelection) == 1){
-            person = person + 1;
-            console.log("Win! " + playerSelection + " beats " + computerSelection + "!")
+            point = 1;
+        result = ("Win! " + playerSelection + " beats " + computerSelection + "!")
         } else if (playRound(playerSelection, computerSelection) == 2) {
-            comp = comp + 1;
-            console.log("Lose! " + playerSelection + " loses to " + computerSelection + "!")
+            point = 2;
+            result = ("Lose! " + playerSelection + " loses to " + computerSelection + "!")
         } else {
-            person = person;
-            console.log ("Tie! " + playerSelection + " cancels " + computerSelection + "!")
+            point = 0;
+            result = ("Tie! " + playerSelection + " cancels " + computerSelection + "!")
         }
+        
+        const div = document.querySelector(".buttons");
+        const results = document.createElement('div');
+        results.textContent = result;
+        div.appendChild(results);
+
+        return point;
+
+    })
+    paperpress.addEventListener('click', () => {
+        console.log('got here too');
+        let playerSelection = 'paper';
+        let computerSelection = getComputerChoice();
+        if (playRound(playerSelection, computerSelection) == 1){
+            point  = 1;
+            result = ("Win! " + playerSelection + " beats " + computerSelection + "!")
+        } else if (playRound(playerSelection, computerSelection) == 2) {
+            point = 2;
+            result = ("Lose! " + playerSelection + " loses to " + computerSelection + "!")
+        } else {
+            result = ("Tie! " + playerSelection + " cancels " + computerSelection + "!")
+        }
+        
+        const div = document.querySelector(".buttons");
+        const results = document.createElement('div');
+        results.textContent = result;
+        div.appendChild(results);
+
+        return point;
+        
+    })
+    scissorspress.addEventListener('click', () => {
+        console.log('got here too');
+        let playerSelection = 'scissors';
+        let computerSelection = getComputerChoice();
+        if (playRound(playerSelection, computerSelection) == 1){
+            point = 1;
+            result = ("Win! " + playerSelection + " beats " + computerSelection + "!")
+        } else if (playRound(playerSelection, computerSelection) == 2) {
+            point = 2;
+            result = ("Lose! " + playerSelection + " loses to " + computerSelection + "!")
+        } else {
+            result = ("Tie! " + playerSelection + " cancels " + computerSelection + "!")
+        }
+        console.log(result);
+        
+        const div = document.querySelector(".buttons");
+        const results = document.createElement('div');
+        results.textContent = result;
+        div.appendChild(results);
+
+        return point;
+        
+    })
     }
-    if (person == comp) {
-        return ("Complete Tie, no winner or loser. Let's keep going!")
-    } else if (person > comp) {
-        return ("You have won! Nicely done!")
+
+console.log("beginning");
+let counter = 0;
+let player = 0;
+let comp = 0;
+//while (counter < 6) {
+    thePoint = game();
+    counter += 1;
+    if (thePoint == 1){
+        player += 1;
+    } else if (thePoint == 2){
+        comp += 1; 
+    }
+    console.log("points" + player + comp)
+//}
+
+    if (player < comp) {
+        console.log("The computer has won! Try again to win!")
+    } else if (player > comp) {
+        console.log("You have won! Nicely done!")
     }
     else {
-        return ("The computer has won! Try again to win!")
+        console.log("Complete Tie, no winner or loser. Let's keep going!")
     }
-}
+//const div = document.querySelector(".buttons");
+//const results = document.createElement('div');
+//results.textContent = displayString;
+//div.appendChild(results);
 
-console.log(game());
+//let displayString = game();
+//console.log(displayString);
